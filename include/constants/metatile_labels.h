8e50af418f61321fe949c1d36191b52951b31273
SphericalIce <sphericalice@outlook.com>
2020-08-18 16:06:29 +0100

    Place events in the Reliquia Ruins

--- include/constants/metatile_labels.h
+++ include/constants/metatile_labels.h
@@ -395,7 +395,6 @@
 #define METATILE_PetalburgGym_SlidingDoor_Frame4  0x21C
 
 // gTileset_Petalburg
-#define METATILE_Petalburg_Door_BirchsLab     0x249
 #define METATILE_Petalburg_Door_Condominiums  0x27A
 #define METATILE_Petalburg_Door_Littleroot    0x248
 
