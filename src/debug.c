89ee6b1963d1c1859f8ab87c682eda5ce38f2641
SphericalIce <sphericalice@outlook.com>
2020-10-02 12:21:56 +0100

    Refine debug fast-forwarding & add party/item adders

--- src/debug.c
+++ src/debug.c
@@ -20,28 +20,40 @@
 
 static void Debug_DestroyMainMenu(u8);
 static void DebugAction_FastForward(u8);
+static void DebugAction_GetParty(u8);
+static void DebugAction_GetItems(u8);
 static void DebugAction_Cancel(u8);
 static void DebugTask_HandleMainMenuInput(u8);
 
 extern const u8 Debug_EventScript_SetUpFastForward[];
+extern const u8 Debug_EventScript_GetParty[];
+extern const u8 Debug_EventScript_GetItems[];
 
 enum {
     DEBUG_MENU_ITEM_FAST_FORWARD,
+    DEBUG_MENU_ITEM_GET_PARTY,
+    DEBUG_MENU_ITEM_GET_ITEMS,
     DEBUG_MENU_ITEM_CANCEL,
 };
 
-static const u8 gDebugText_FastForward[] = _("Fast Forward");
+static const u8 gDebugText_FastForward[] = _("Fast forward");
+static const u8 gDebugText_GetParty[] = _("Get party");
+static const u8 gDebugText_GetItems[] = _("Get items");
 static const u8 gDebugText_Cancel[] = _("Cancel");
 
 static const struct ListMenuItem sDebugMenuItems[] =
 {
     [DEBUG_MENU_ITEM_FAST_FORWARD] = {gDebugText_FastForward, DEBUG_MENU_ITEM_FAST_FORWARD},
+    [DEBUG_MENU_ITEM_GET_PARTY] = {gDebugText_GetParty, DEBUG_MENU_ITEM_GET_PARTY},
+    [DEBUG_MENU_ITEM_GET_ITEMS] = {gDebugText_GetItems, DEBUG_MENU_ITEM_GET_ITEMS},
     [DEBUG_MENU_ITEM_CANCEL] = {gDebugText_Cancel, DEBUG_MENU_ITEM_CANCEL}
 };
 
 static void (*const sDebugMenuActions[])(u8) =
 {
     [DEBUG_MENU_ITEM_FAST_FORWARD] = DebugAction_FastForward,
+    [DEBUG_MENU_ITEM_GET_PARTY] = DebugAction_GetParty,
+    [DEBUG_MENU_ITEM_GET_ITEMS] = DebugAction_GetItems,
     [DEBUG_MENU_ITEM_CANCEL] = DebugAction_Cancel
 };
 
@@ -136,6 +148,18 @@ static void DebugAction_FastForward(u8 taskId)
     ScriptContext2_RunNewScript(Debug_EventScript_SetUpFastForward);
     SetWarpDestination(MAP_GROUP(WINDMIST_CITY), MAP_NUM(WINDMIST_CITY), 0, 0, 0);
     DoWarp();
+}
+
+static void DebugAction_GetParty(u8 taskId)
+{
+    Debug_DestroyMainMenu(taskId);
+    ScriptContext2_RunNewScript(Debug_EventScript_GetParty);
+}
+
+static void DebugAction_GetItems(u8 taskId)
+{
+    Debug_DestroyMainMenu(taskId);
+    ScriptContext2_RunNewScript(Debug_EventScript_GetItems);
 }
 
 static void DebugAction_Cancel(u8 taskId)
