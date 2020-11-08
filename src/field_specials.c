99a07c13766d1653ea97a2f484c6ceec80b347c3
SphericalIce <sphericalice@outlook.com>
2020-11-08 16:43:57 +0000

    Begin removing references to old hidden item flags

--- src/field_specials.c
+++ src/field_specials.c
@@ -1165,7 +1165,7 @@ void EndLotteryCornerComputerEffect(void)
 void SetTrickHouseNuggetFlag(void)
 {
     u16 *specVar = &gSpecialVar_0x8004;
-    u16 flag = FLAG_HIDDEN_ITEM_TRICK_HOUSE_NUGGET;
+    u16 flag = 0;
     *specVar = flag;
     FlagSet(flag);
 }
@@ -1173,7 +1173,7 @@ void SetTrickHouseNuggetFlag(void)
 void ResetTrickHouseNuggetFlag(void)
 {
     u16 *specVar = &gSpecialVar_0x8004;
-    u16 flag = FLAG_HIDDEN_ITEM_TRICK_HOUSE_NUGGET;
+    u16 flag = 0;
     *specVar = flag;
     FlagClear(flag);
 }
