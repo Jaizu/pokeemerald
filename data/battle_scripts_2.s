3b4daa68a9b4ae966571657fc8d92e80a55ede8b
SphericalIce <sphericalice@outlook.com>
2021-01-02 21:45:08 +0000

    Fix some spaces and tabs

--- data/battle_scripts_2.s
+++ data/battle_scripts_2.s
@@ -14,43 +14,43 @@
 
 	.align 2
 gBattlescriptsForBallThrow:: @ 82DBD08
-	.4byte BattleScript_BallThrow        @ ITEM_NONE
-	.4byte BattleScript_BallThrow        @ ITEM_MASTER_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_ULTRA_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_GREAT_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_POKE_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_NONE
+	.4byte BattleScript_BallThrow		@ ITEM_MASTER_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_ULTRA_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_GREAT_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_POKE_BALL
 	.4byte BattleScript_SafariBallThrow  @ ITEM_SAFARI_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_NET_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_DIVE_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_NEST_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_REPEAT_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_TIMER_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_LUXURY_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_DUSK_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_HEAL_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_QUICK_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_CHERISH_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_FAST_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_LEVEL_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_LURE_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_HEAVY_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_LOVE_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_FRIEND_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_MOON_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_SPORT_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_PARK_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_DREAM_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_BEAST_BALL
-	.4byte BattleScript_BallThrow        @ ITEM_PREMIER_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_NET_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_DIVE_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_NEST_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_REPEAT_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_TIMER_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_LUXURY_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_DUSK_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_HEAL_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_QUICK_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_CHERISH_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_FAST_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_LEVEL_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_LURE_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_HEAVY_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_LOVE_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_FRIEND_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_MOON_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_SPORT_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_PARK_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_DREAM_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_BEAST_BALL
+	.4byte BattleScript_BallThrow		@ ITEM_PREMIER_BALL
 
 	.align 2
 gBattlescriptsForUsingItem:: @ 82DBD3C
 	.4byte BattleScript_PlayerUsesItem
-	.4byte BattleScript_OpponentUsesHealItem        @ AI_ITEM_FULL_RESTORE
-	.4byte BattleScript_OpponentUsesHealItem        @ AI_ITEM_HEAL_HP
+	.4byte BattleScript_OpponentUsesHealItem		@ AI_ITEM_FULL_RESTORE
+	.4byte BattleScript_OpponentUsesHealItem		@ AI_ITEM_HEAL_HP
 	.4byte BattleScript_OpponentUsesStatusCureItem  @ AI_ITEM_CURE_CONDITION
-	.4byte BattleScript_OpponentUsesXItem           @ AI_ITEM_X_STAT
-	.4byte BattleScript_OpponentUsesGuardSpecs      @ AI_ITEM_GUARD_SPECS
+	.4byte BattleScript_OpponentUsesXItem		   @ AI_ITEM_X_STAT
+	.4byte BattleScript_OpponentUsesGuardSpecs	  @ AI_ITEM_GUARD_SPECS
 
 	.align 2
 gBattlescriptsForRunningByItem:: @ 82DBD54
@@ -121,7 +121,7 @@ BattleScript_ShakeBallThrow::
 	printstring STRINGID_OUTOFSAFARIBALLS
 	waitmessage 0x40
 	setbyte gBattleOutcome, B_OUTCOME_NO_SAFARI_BALLS
-    goto BattleScript_ShakeBallThrowEnd
+	goto BattleScript_ShakeBallThrowEnd
 BattleScript_ParkBallsCheck:
 	jumpifword CMP_NO_COMMON_BITS, gBattleTypeFlags, BATTLE_TYPE_BUG_CATCHING_CONTEST, BattleScript_ShakeBallThrowEnd
 	jumpifbyte CMP_NOT_EQUAL, gNumParkBalls, 0x0, BattleScript_ShakeBallThrowEnd
