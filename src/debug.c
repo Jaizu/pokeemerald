ba78fe4dee444e43cb8d77e8dcc97a2df7c771b5
SphericalIce <sphericalice@outlook.com>
2021-01-23 22:19:26 +0000

    Update the debug script

--- src/debug.c
+++ src/debug.c
@@ -146,7 +146,7 @@ static void DebugAction_FastForward(u8 taskId)
 {
     Debug_DestroyMainMenu(taskId);
     ScriptContext2_RunNewScript(Debug_EventScript_SetUpFastForward);
-    SetWarpDestination(MAP_GROUP(VALOON_TOWN_GYM), MAP_NUM(VALOON_TOWN_GYM), 0, 0, 0);
+    SetWarpDestination(MAP_GROUP(VALOON_TOWN_GATEHOUSE_1F), MAP_NUM(VALOON_TOWN_GATEHOUSE_1F), 1, 0, 0);
     DoWarp();
 }
 
