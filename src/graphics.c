fb1bb542b06625206db3858951fe63aede0e030a
Jaizu <jaizu85@gmail.com>
2025-06-12 11:35:11 +0200

    Implement Anti-Piracy screen

--- src/graphics.c
+++ src/graphics.c
@@ -1257,6 +1257,10 @@ const u16 gIntroCopyright_Pal[] = INCBIN_U16("graphics/intro/copyright.gbapal");
 const u32 gIntroCopyright_Gfx[] = INCBIN_U32("graphics/intro/copyright.4bpp.lz");
 const u32 gIntroCopyright_Tilemap[] = INCBIN_U32("graphics/intro/copyright.bin.lz");
 
+const u16 gIntroAntiPiracy_Pal[] = INCBIN_U16("graphics/intro/antipiracy/palette.gbapal");
+const u32 gIntroAntiPiracy_Gfx[] = INCBIN_U32("graphics/intro/antipiracy/tileset.4bpp.lz");
+const u32 gIntroAntiPiracy_Tilemap[] = INCBIN_U32("graphics/intro/antipiracy/tilemap.bin.lz");
+
 const u16 gPokedexAreaScreenAreaUnknown_Pal[] = INCBIN_U16("graphics/pokedex/area_unknown.gbapal");
 const u32 gPokedexAreaScreenAreaUnknown_Gfx[] = INCBIN_U32("graphics/pokedex/area_unknown.4bpp.lz");
 
